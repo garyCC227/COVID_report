a4995c8c0e39510fe5340531da42ae5fea1d7ebe
yahnis <yang.zheng@student.unsw.edu.au>
2020-03-11 15:45:06 +1100

    add instruction for manual testing

--- PHASE_1/NLP_PhaseMatcher_version/TestDateFormater.py
+++ PHASE_1/NLP_PhaseMatcher_version/TestDateFormater.py
@@ -1,12 +1,46 @@
 from Date_Formater import Date_Formater 
 
-if __name__ == "__main__":
+def return_date_string() :
     test = Date_Formater()
     while (1) :
         val = input("Enter Date or \"q\" to quit: ")
         if val == "q" :
+            print("Thank you for testing")
             break
         else :
-            print(test.match_date(val))
+            print(test.match_date(val))  
+    val = input("Test again? (y/n)")
+    if val == "y" :
+        return_date_string()
+
+def return_date_period() :
+    print("Type in string of date one by one, and \"q\" to stop\nThe result should be a period of date or a single date\nThe period should be as long as posible\nAnd the date should be as accurate as possible ")
+    test = Date_Formater()
+    while (1) :
+        val = input("Enter Date or \"q\" to stop: ")
+        print(test.match_date(val))
+        if val == "q" :
+            print("Thank you for testing")
+            break
+        else :
             test.add_date(val)
     print(test.get_event_date())
+    val = input("Test again? (y/n)")
+    if val == "y" :
+        return_date_period()
+
+if __name__ == "__main__":
+    test = Date_Formater()
+    val = input("Welcome to manual test platform\nEnter \"1\" for single date formater\nEnter \"2\" for a set of date\nEnter \"q\" to quit ")
+    while (1) :
+        if val == "1" :
+            return_date_string()
+            break
+        elif val == "2" :
+            return_date_period()
+            break
+        elif val == "q" :
+            print("Thank you for testing")
+            break
+        else :
+            val = input("Welcome to manual test platform\nEnter \"1\" for single date formater\nEnter \"2\" for a set of date\nEnter \"q\" to quit ")

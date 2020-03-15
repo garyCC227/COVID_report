ca2d362b413e63f9a4a3e5ea63ddf58dac9186fb
Wenlue Zhang <z5158333@ad.unsw.edu.au>
2020-03-15 09:38:37 +0800

    Fix: incorrectly passing parameter

--- PHASE_1/API_SourceCode/Source/Application/Actions/GetNewsByIdAction.py
+++ PHASE_1/API_SourceCode/Source/Application/Actions/GetNewsByIdAction.py
@@ -5,4 +5,4 @@ class GetNewsByIdAction:
 
     def __new__(cls, id):
         retriever = FirebaseDocumentRetriever()
-        return retriever.get_document_by_id(1)
+        return retriever.get_document_by_id(id)

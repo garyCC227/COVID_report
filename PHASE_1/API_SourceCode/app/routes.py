f9d70ab6e29df72df71032ced54c2aa96a579f69
Wenlue Zhang <z5158333@ad.unsw.edu.au>
2020-03-15 09:06:47 +0800

    Feat: server can catch domain not found error

--- PHASE_1/API_SourceCode/app/routes.py
+++ PHASE_1/API_SourceCode/app/routes.py
@@ -4,6 +4,7 @@ from Source.Application.Actions.GetNewsByIdAction import GetNewsByIdAction
 from Source.Application.Actions.GetReportByIdAction import GetReportByIdAction
 from Source.Application.Actions.GetNewsListAction import GetNewsListAction
 from Source.Infrastructure.Loggers.AccessLogger import AccessLogger
+from Source.Domain.Exceptions.NotFoundException import NotFoundException
 from flask import request
 
 
@@ -15,6 +16,10 @@ def check_identity():
     else:
         logger = AccessLogger()
         logger.write(identity, request.path)
+
+@app.errorhandler(NotFoundException)
+def handle_bad_request(e):
+    return make_response(e, e.code)
 
 @app.route("/")
 def hello():

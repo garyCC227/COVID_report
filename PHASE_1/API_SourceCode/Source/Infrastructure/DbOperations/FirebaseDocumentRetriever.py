5c29f10e2ca3027b16afd89a4115878988775a43
Wenlue Zhang <z5158333@ad.unsw.edu.au>
2020-03-17 17:52:19 +0800

    Fix: introduce database filter

--- PHASE_1/API_SourceCode/Source/Infrastructure/DbOperations/FirebaseDocumentRetriever.py
+++ PHASE_1/API_SourceCode/Source/Infrastructure/DbOperations/FirebaseDocumentRetriever.py
@@ -1,5 +1,5 @@
 from .FirebaseListFactory import FirebaseListFactory
-from .Filter import Filter
+from .FirebaseDocumentFilterFactory import FirebaseDocumentFilterFactory
 from ...DomainFactory.ReportListFactory import ReportListFactory
 from ...DomainFactory.ReportFactory import ReportFactory
 from ...DomainFactory.NewsFactory import NewsFactory
@@ -24,16 +24,6 @@ class FirebaseDocumentRetriever():
         else:
             return factory.make(db_result)
 
-    def _create_filter(self, list):
-        product = Filter(list)
-        if "start_date" in self._criteria and "end_date" in self._criteria:
-            product.set_date_range(self._criteria["start_date"], self._criteria["end_date"])
-        if "location" in self._criteria:
-            product.set_location(self._criteria["location"])
-        if "keyterms" in self._criteria:
-            product.set_location(self._criteria["keyterms"])
-        return product
-
     def get_document_by_id(self, id):
         db = firestore.client()
 
@@ -45,33 +35,13 @@ class FirebaseDocumentRetriever():
         result['id'] = doc.id
         return self._create_document(result, with_report=True)
 
-    def get_documents_by_location(self, location):
-        db = firestore.client()
-        query = db.collection(u'reports').where(u'locations', u'array_contains', location)
-        handler = lambda elem: self._create_document(elem, shorten=True)
-        result_list = FirebaseListFactory().make(query)
-        return list(map(handler, result_list))
-
-    def get_documents_by_syndrome(self, syndrome):
-        db = firestore.client()
-        query = db.collection(u'reports').where(u'syndromes', u'array_contains', syndrome)
-        handler = lambda elem: self._create_document(elem, shorten=True)
-        result_list = FirebaseListFactory().make(query)
-        return list(map(handler, result_list))
-
-    def get_documents_by_disease(self, disease):
-        db = firestore.client()
-        query = db.collection(u'reports').where(u'diseases', u'array_contains', disease)
-        handler = lambda elem: self._create_document(elem, shorten=True)
-        result_list = FirebaseListFactory().make(query)
-        return list(map(handler, result_list))
-
     def get_all_documents(self, complete_version=False):
+        filter = FirebaseDocumentFilterFactory().make(self._criteria)
         db = firestore.client()
         query = db.collection(u'reports')
+        query = filter.apply(query)
         handler = lambda elem: self._create_document(elem, shorten=not complete_version)
         result_list = FirebaseListFactory().make(query)
-        result_list = self._create_filter(result_list).execute()
         return list(map(handler, result_list))
 
     def get_report_by_id(self, id):

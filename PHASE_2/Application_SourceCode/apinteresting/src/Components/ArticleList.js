1ed68279feae92ade58a92ba195ac889ec7ad78c
Linchen Chen <lin.chen@student.unsw.edu.au>
2020-04-03 19:15:53 +1100

    articles list almost done

--- PHASE_2/Application_SourceCode/apinteresting/src/Components/ArticleList.js
+++ PHASE_2/Application_SourceCode/apinteresting/src/Components/ArticleList.js
@@ -7,18 +7,18 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-  inline: {
-    display: 'inline',
-  },
-  previewText: {
-    color: '#000',
-  }
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: '100%',
+//     backgroundColor: theme.palette.background.paper,
+//   },
+//   inline: {
+//     display: 'inline',
+//   },
+//   previewText: {
+//     color: '#000',
+//   }
+// }));
 
 
 const data = {
@@ -30,58 +30,50 @@ const data = {
 }
 
 
-export default function ArticleList() {
-  const classes = useStyles();
-  const url = 'https://apinteresting.xyz/v1/news?start_date=2020-02-15T00%3A00%3A00&end_date=2020-03-01T00%3A00%3A00&keyterms=coronavirus%2Cflu&location=China';
-  const lists = fetch(url, 
-                    {
-                      method:'GET',
-                      headers: {'identity':'header'
-                                  }
-                    })
-                .then(res=>{
-                  console.log(res);
-                  return res.json;
-                })
-                .then(res=>{
-                  return res.data;
-                })
-  // articles
-  const artiles = [...Array(5)].map(function(){ 
-   return(
-     <div>
-      <ListItem alignItems="flex-start">
-        <ListItemText
-          primary={
-            <div>
-              <Link href="https://www.google.com/" color="inherit">
-                <h3>{data.headline}</h3>
-              </Link>
-            </div>
-          }
-          secondary={
-            <React.Fragment>
-              {data.date_of_publication}
-              <span> - </span>
-              <Link href={data.url}>{data.url}</Link>
-              <br />
-              <p className={classes.previewText}>{data.main_text}</p>
-            </React.Fragment>
-          }
-        />
-    </ListItem>
-    <Divider component="li"/>
-    </div>
-      );
-  });
-  
-  // console.log(lists);
-  return (
-    <Box m={1}>
-      <List className={classes.root}>
-        {artiles}
-        {/* {lists} */}
-      </List>
-    </Box>
-  );
+class ArticleList extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    const articles = this.props.articles.map(function(){ 
+      return(
+        <div>
+         <ListItem alignItems="flex-start">
+           <ListItemText
+             primary={
+               <div>
+                 <Link href="https://www.google.com/" color="inherit">
+                   <h3>{data.headline}</h3>
+                 </Link>
+               </div>
+             }
+             secondary={
+               <React.Fragment>
+                 {data.date_of_publication}
+                 <span> - </span>
+                 <Link href={data.url}>{data.url}</Link>
+                 <br />
+                 <p className="">{data.main_text}</p>
+               </React.Fragment>
+             }
+           />
+       </ListItem>
+       <Divider component="li"/>
+       </div>
+         );
+     });
+
+    return(
+      <Box m={1}>
+        <List className="">
+          {articles}
+          {/* {lists} */}
+        </List>
+      </Box>
+    );
+  }
 }
+
+
+export default ArticleList;
--- PHASE_2/Application_SourceCode/apinteresting/src/Components/ArticleListPage.js
+++ PHASE_2/Application_SourceCode/apinteresting/src/Components/ArticleListPage.js
@@ -1,21 +1,71 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Divider from '@material-ui/core/Divider';
-import ArticleSearchToolbar from './ArticleSearchToolbar';
-import ArticleList from './ArticleList';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import ArticleSearchToolbar from "./ArticleSearchToolbar";
+import ArticleList from "./ArticleList";
 
-const useStyles = makeStyles((theme) => ({
-}));
+const data = {
+  id: "046fu6uh96V9zlcVlUTK",
+  url:
+    "http://www.cidrap.umn.edu/news-perspective/2020/02/canada-lebanon-report-iran-linked-covid-19-cases-concerns-rise",
+  date_of_publication: "2020-02-21 23:05:21",
+  headline:
+    "Canada, Lebanon report Iran-linked COVID-19 cases as concerns rise",
+  main_text:
+    "University of Minnesota. Driven to Discover.Following recent reports of COV..."
+};
 
+class ArticleListPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: this.initValue()
+    };
 
-export default function ArticleListPage() {
-  const classes = useStyles();
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+  }
 
-  return (
-    <div>
-      <ArticleSearchToolbar />
-      <Divider />
-      <ArticleList />
-    </div>
-  );
+  //TODO:
+  initValue() {
+    const url =
+      "https://apinteresting.xyz/v1/news?start_date=2020-02-15T00%3A00%3A00&end_date=2020-03-01T00%3A00%3A00&keyterms=coronavirus%2Cflu&location=China";
+    const lists = fetch(url, {
+      method: "GET",
+      headers: { identity: "header" }
+    })
+      .then(res => {
+        console.log(res);
+        return res.json;
+      })
+      .then(res => {
+        return res.data;
+      });
+
+    console.log(lists);
+    return [data];
+  }
+
+  //change to fetch later
+  onSearchSubmit(search_form) {
+    //TODO: change to fetch later
+    console.log(
+      search_form.start_date,
+      search_form.end_date,
+      search_form.location,
+      search_form.keyword
+    );
+    this.setState({ articles: [data, data, data] });
+  }
+
+  render() {
+    return (
+      <div>
+        <ArticleSearchToolbar onSubmit={this.onSearchSubmit} />
+        <Divider />
+        <ArticleList articles={this.state.articles} />
+      </div>
+    );
+  }
 }
+
+export default ArticleListPage;

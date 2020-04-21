854e60bbe862f3c144634dadf986f49899c461bc
Linchen Chen <lin.chen@student.unsw.edu.au>
2020-04-21 18:49:58 +1000

    add some trending

--- PHASE_2/Application_SourceCode/apinteresting/src/Components/GoogleTrendChart.js
+++ PHASE_2/Application_SourceCode/apinteresting/src/Components/GoogleTrendChart.js
@@ -1,5 +1,5 @@
-import ScriptTag from 'react-script-tag';
-import React, { Component } from "react"
+import ScriptTag from "react-script-tag";
+import React, { Component } from "react";
 
 export default class GoogleTrendChart extends Component {
   constructor(props) {
@@ -10,27 +10,14 @@ export default class GoogleTrendChart extends Component {
     return (
       <div id="google-trend-container">
         <ScriptTag type="text/javascript">
-          {
-            `
+          {`
             const root = document.getElementById('google-trend-container');
-            trends.embed.renderExploreWidgetTo(
+            trends.embed.renderWidgetTo(
               root,
-              "TIMESERIES",
-              {
-                "comparisonItem": [
-                    {
-                        "keyword": "Corona",
-                        "geo": "",
-                        "time": "today 3-m"
-                    }
-                ],
-                "category": 0,
-                "property": ""
-            },
-            {
-                "guestPath": "https://trends.google.com:443/trends/embed/"
-            })`
-          }
+              "US_cu_4Rjdh3ABAABMHM_en_en-AU", 
+              "fe_line_chart_c57f6363-decb-4fe9-8f3a-e05ec0b99e0f_en-AU", 
+              {"guestPath":"https://trends.google.com:443/trends/embed/"}); 
+            `}
         </ScriptTag>
       </div>
     );

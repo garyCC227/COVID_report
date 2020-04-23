12728e64b1129a4d5808b24e1dd33f69057146c6
Wenlue Zhang <z5158333@ad.unsw.edu.au>
2020-04-23 17:30:23 +0800

    Beautify Article List

--- PHASE_2/Application_SourceCode/apinteresting/src/Components/ArticleList.js
+++ PHASE_2/Application_SourceCode/apinteresting/src/Components/ArticleList.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import ArticleDialog from './ArticleDialog';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     width: '100%',
-//     backgroundColor: theme.palette.background.paper,
-//   },
-//   inline: {
-//     display: 'inline',
-//   },
-//   previewText: {
-//     color: '#000',
-//   }
-// }));
-
+import { Card, CardContent, Typography, CardActions } from '@material-ui/core';
 
 class ArticleList extends React.Component {
   constructor() {
@@ -31,43 +14,35 @@ class ArticleList extends React.Component {
   render() {
     const articles = this.props.articles.map(function (data) {
       return (
-        <div key={data.id}>
-          <ListItem alignItems="flex-start">
-            <ListItemText
-              primary={
-                <div>
-                  <Link href={data.url} color="inherit" target="_blank">
-                    <h3>{data.headline}</h3>
-                  </Link>
-
-                </div>
-              }
-              secondary={
-                <React.Fragment>
-                  {data.date_of_publication}
-                  <br />
-                  <p>{data.main_text}</p>
-                  <Button size="small" href={data.url} target="_blank" variant="outlined" color="primary">
-                    View Article
-                  </Button>
-                  &nbsp;
-                  <ArticleDialog article={data} />
-                </React.Fragment>
-
-              }
-            />
-          </ListItem>
-          <Divider component="li" />
-        </div>
+        <Box mb={1} key={data.id}>
+          <Card>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {data.headline}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {data.date_of_publication}
+                <br />
+                {data.main_text}
+              </Typography>
+            </CardContent>
+            <Divider />
+            <CardActions>
+              <Button size="small" href={data.url} target="_blank" variant="outlined" color="primary">
+                View Article
+              </Button>
+              &nbsp;
+              <ArticleDialog article={data} />
+            </CardActions>
+          </Card>
+        </Box>
       );
     });
 
     return (
-      <Box m={1}>
-        <List className="">
-          {articles}
-        </List>
-      </Box>
+      <List className="">
+        {articles}
+      </List>
     );
   }
 }

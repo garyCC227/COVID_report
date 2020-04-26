39169f69978336e70870dce03a377640af6ca42c
Wenlue Zhang <z5158333@ad.unsw.edu.au>
2020-04-26 23:13:32 +0800

    Style fix for Twitter trends

--- PHASE_2/Application_SourceCode/apinteresting/src/Components/TwitterTrendsPage.js
+++ PHASE_2/Application_SourceCode/apinteresting/src/Components/TwitterTrendsPage.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import firebase from '../fbconfig'
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import Card from "./Style/Card.js";
 import CardHeader from "./Style/CardHeader.js";
 import CardBody from "./Style/CardBody.js";
 import Typography from '@material-ui/core/Typography';
@@ -26,10 +25,7 @@ const styles = ({
     fontSize: 14,
   },
   pos: {
-    marginBottom: 12,
-  },
-  media: {
-    height: 140,
+    marginBottom: -1,
   },
 });
 
@@ -42,12 +38,12 @@ class TwitterTrendsPage extends Component {
     isLoading: true,
   }
 
-  componentWillMount () {
+  componentWillMount() {
     const script = document.createElement("script");
- 
+
     script.src = 'https://hashtagify.me/assets/hashtagify/embed.js';
     script.async = true;
- 
+
     document.body.appendChild(script);
   }
 
@@ -80,38 +76,53 @@ class TwitterTrendsPage extends Component {
     const { classes } = this.props
     return (
       <div>
-      <div>
-        {this.state.isLoading ? <div style={{ textAlign: "center" }}><this.LoadingIndicator /></div> : <Card>
-          <CardHeader color="info">
-            <h2>Twitter Trends</h2>
-          </CardHeader>
-          <CardBody />
-        </Card>}
-        {(this.state.twitterPosts).filter((key,i) => i < 5).map((item) => {
-          return (
-            <div key={item.id}>
-              <Card className={classes.root} variant="outlined">
-                <CardContent>
-                  <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    {item.user['name']}
+        <div>
+          {
+            this.state.isLoading
+              ?
+              <div style={{ textAlign: "center" }}>
+                <this.LoadingIndicator />
+              </div>
+              :
+              <Card>
+                <CardHeader color="info">
+                  <h2>Twitter Trends</h2>
+                </CardHeader>
+                <CardBody>
+                  <Typography variant="h6">
+                    Latest Tweets
                   </Typography>
-                  <Typography className={classes.pos} color="textSecondary">
-                    Retweeted: <b>{item.retweet_count} times</b>
-                  </Typography>
+                  {(this.state.twitterPosts).filter((key, i) => i < 5).map((item) => {
+                    return (
+                      <Card variant="outlined" style={{ marginTop: "0.5rem", marginBottom: "0.5rem" }}>
+                        <CardBody>
+                          <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            {item.user['name']}
+                          </Typography>
+                          <Typography className={classes.pos} color="textSecondary">
+                            Retweeted: <b>{item.retweet_count} times</b>
+                          </Typography>
 
-                  <Typography variant="body2" component="p">
-                    {item.full_text}
-                    <br />
-                  </Typography>
-                </CardContent>
-
+                          <Typography variant="body2" component="p">
+                            {item.full_text}
+                            <br />
+                          </Typography>
+                        </CardBody>
+                      </Card>
+                    )
+                  })}
+                </CardBody>
               </Card>
-              <br />
-            </div>
-          )
-        })}
-      </div>
-      <div class="hastagify_embed" data-hashtag="Disease" data-width="1600" data-mode="table"><div><a href="http://hashtagify.me/"></a></div></div>
+
+          }
+        </div>
+
+        <div className="hastagify_embed" data-hashtag="Disease" data-width="max" data-mode="table">
+          <div>
+            <a href="http://hashtagify.me/"></a>
+          </div>
+        </div>
+
       </div>);
   }
 }
